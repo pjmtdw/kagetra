4e98aa9c74b26c90d14624c52d5c7d51554b3126
Haruhiro Yoshimoto <hpnpwd@gmail.com>
2014-10-29 15:38:35 +0900

    added helper module for migration

---
+++ migrate/helper.rb
@@ -0,0 +1,36 @@
+
+module Sequel
+  class Database
+    CUSTOM_EXTRA_BLOCKS = {
+      base: lambda{|x|
+        primary_key :id
+        DateTime :created_at, index:true, null:false
+        DateTime :updated_at, index:true, null:false
+      }
+    }
+    def create_table_custom(name, extra_blocks, options=OPTS, &block)
+      create_table(name, options){
+        extra_blocks.each{|b|
+          instance_eval(&CUSTOM_EXTRA_BLOCKS[b])
+        }
+        instance_eval(&block) if block
+      }
+    end
+  end
+  # this code adds sequel to insert comments
+  # see lib/sequel/database/schema_methods.rb and lib/sequel/adapters/shared/mysql.rb
+  module MySQL
+    module DatabaseMethods
+      COLUMN_DEFINITION_ORDER.insert(COLUMN_DEFINITION_ORDER.index(:auto_increment)+1,:comment)
+      def column_definition_comment_sql(sql, column)
+        sql << " COMMENT #{literal(column[:comment])}" if column.include?(:comment)
+      end
+      # http://stackoverflow.com/questions/4470108/when-monkey-patching-a-method-can-you-call-the-overridden-method-from-the-new-i
+      original_create_table_sql = instance_method(:create_table_sql)
+      define_method(:create_table_sql){ |name, generator, options = OPTS |
+        comment = options.fetch(:comment, nil)
+        "#{original_create_table_sql.bind(self).(name,generator,options)}#{ " COMMENT='#{comment}'" if comment }"
+      }
+    end
+  end
+end

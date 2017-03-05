533b560bbc14e19fb3edf33a390ba9a7b62c98eb
Haruhiro Yoshimoto <pjmtdw@gmail.com>
2017-03-05 15:01:02 +0900

    read only bbs with single page

--- vue/components/bbs.vue
+++ vue/components/bbs.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class='hello'>{{message}}</div>
+  <div class='mx-auto' style='width:45em;'>
+    <div class='card thread' v-for='thread in threads'>
+      <div class='card-header'>
+        {{thread.title}}
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class='list-group-item' v-for='item in thread.items'>
+          <div>
+            <span class='name'>{{item.user_name}}</span>＠<span class='date'>{{item.date}}</span>
+          </div>
+          {{item.body}}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
 export default {
+  created() {
+    axios.get('/api/bbs/threads?page=1').then((res) => {
+      this.threads = res.data;
+    });
+  },
   data() {
     return {
-      message: '掲示板',
+      threads: [],
     };
   },
 };
 </script>
-<style>
-.hello {
-  color: red;
+<style scoped>
+.thread {
+  white-space: pre-line;
+  margin-bottom: 1.5em;
+}
+
+.name {
+  color: #040;
+}
+
+.date {
+  color: brown;
 }
 </style>

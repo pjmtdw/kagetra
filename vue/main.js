22e13c25ff81fbdf6611e3f235f20022eda2b302
Awaka Ryohei <bubble0601@hotmail.co.jp>
2018-02-22 14:36:24 +0900

    split main.js

--- vue/main.js
+++ vue/main.js
@@ -9,6 +9,8 @@ import schedule from './routers/schedule';
 import top from './routers/top';
 import wiki from './routers/wiki';
 
+import misc from './misc';
+
 Vue.use(VueRouter);
 
 const routes = {
@@ -22,72 +24,14 @@ const routes = {
 };
 
 /* global location */
-const route = routes[location.pathname.substr(1)];
+const routeName = location.pathname.split('/')[1];
+const route = routes[routeName];
 
-// for navbar
-$(`nav #${location.pathname.substr(1)}`).addClass('active');
-
-// 通知機能追加
-// @param type 通知のスタイル(bootstrapのalert準拠. primaryなど)
-// @param message 通知の内容
-$.notify = (type, message) => {
-  // クリア
-  if (type === 'clear') {
-    $('.notify').remove();
-    return;
-  }
-  // 重ならないようにずらす関数
-  const alignNotify = () => {
-    $('.notify').css('top', (i) => {
-      let height = 0;
-      $(`.notify:lt(${i})`).each((j, e) => {
-        height += $(e).outerHeight(true);
-      });
-      return height;
-    });
-  };
-
-  const $notify = $('<div role="alert"></div>').addClass(`notify alert alert-${type} alert-dismissible fade show w-25 m-1`)
-    .css({
-      position: 'absolute',
-      'z-index': 200,
-      right: '10px',
-      'box-shadow': '5px 5px 10px 0 rgba(0, 0, 0, 0.2)',
-      'min-width': '150px',
-    })
-    .html(message)
-    .append($('<button class="close p-0 type="button" data-dismiss="alert" aria-label="Close"></button>')
-      .css({ top: 0, right: 0 })
-      .html('<span aria-hidden="true">&times;</span>'))
-    .prependTo('#app');
-
-  alignNotify();
-  $notify.on('closed.bs.alert', alignNotify);
-
-  // 30秒後に消える
-  setTimeout(() => { $notify.remove(); }, 30 * 1000);
-};
-
-// formの値をobject化
-$.fn.extend({
-  serializeObject() {
-    const res = {};
-    _.forEach(this.serializeArray(), (v) => {
-      res[v.name] = v.value;
-    });
-    return res;
-  },
-});
+misc(routeName);
 
 const router = new VueRouter({
   routes: route,
   base: location.pathname,
-});
-
-
-axios.interceptors.response.use(null, (error) => {
-  $.notify('danger', `エラー: ${error.response.data.error_message}`);
-  return Promise.reject(error);
 });
 
 /* eslint-disable no-new */
---
+++ vue/misc.js
@@ -0,0 +1,61 @@
+export default (routeName) => {
+  // for navbar
+  $(`nav #${routeName}`).addClass('active');
+
+  // 通知機能追加
+  // @param type 通知のスタイル(bootstrapのalert準拠. primaryなど)
+  // @param message 通知の内容
+  $.notify = (type, message) => {
+    // クリア
+    if (type === 'clear') {
+      $('.notify').remove();
+      return;
+    }
+    // 重ならないようにずらす関数
+    const alignNotify = () => {
+      $('.notify').css('top', (i) => {
+        let height = 0;
+        $(`.notify:lt(${i})`).each((j, e) => {
+          height += $(e).outerHeight(true);
+        });
+        return height;
+      });
+    };
+
+    const $notify = $('<div role="alert"></div>').addClass(`notify alert alert-${type} alert-dismissible fade show w-25 m-1`)
+      .css({
+        position: 'absolute',
+        'z-index': 200,
+        right: '10px',
+        'box-shadow': '5px 5px 10px 0 rgba(0, 0, 0, 0.2)',
+        'min-width': '150px',
+      })
+      .html(message)
+      .append($('<button class="close p-0 type="button" data-dismiss="alert" aria-label="Close"></button>')
+        .css({ top: 0, right: 0 })
+        .html('<span aria-hidden="true">&times;</span>'))
+      .prependTo('#app');
+
+    alignNotify();
+    $notify.on('closed.bs.alert', alignNotify);
+
+    // 10秒後に消える
+    setTimeout(() => { $notify.remove(); }, 10 * 1000);
+  };
+
+  // formの値をobject化
+  $.fn.extend({
+    serializeObject() {
+      const res = {};
+      _.forEach(this.serializeArray(), (v) => {
+        res[v.name] = v.value;
+      });
+      return res;
+    },
+  });
+
+  axios.interceptors.response.use(null, (error) => {
+    $.notify('danger', `エラー: ${error.response.data.error_message}`);
+    return Promise.reject(error);
+  });
+};

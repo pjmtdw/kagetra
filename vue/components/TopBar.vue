7fa040bdbd819f803b1e1308a74f0d00ff05ce86
Awaka Ryohei <bubble0601@hotmail.co.jp>
2018-04-30 18:37:05 +0900

    topbar

--- vue/components/TopBar.vue
+++ vue/components/TopBar.vue
@@ -1,19 +1,18 @@
 <template>
   <nav v-once class="navbar navbar-expand-md navbar-light bg-light" :class="{public: isPublic}">
-    <!-- <span class="navbar-brand mb-0">景虎</span> -->
     <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"/>
     </button>
     <div v-if="!isPublic" id="navbarContent" class="collapse navbar-collapse">
       <ul class="navbar-nav mr-auto">
-        <li v-for="e in navList" :key="e.alias" class="nav-item mx-2">
+        <li v-for="e in list" :key="e.alias" class="nav-item mx-2">
           <a class="nav-link" :class="{active: routeName === e.alias}" :href="`/${e.alias}`">{{ e.name }}</a>
         </li>
       </ul>
     </div>
     <div v-else id="navbarContent" class="collapse navbar-collapse">
       <ul class="navbar-nav mr-auto">
-        <li v-for="e in navList" :key="e.alias" class="nav-item mx-2">
+        <li v-for="e in publicList" :key="e.alias" class="nav-item mx-2">
           <a class="nav-link" :class="{active: routeName === e.alias}" :href="`/public/${e.alias}`">{{ e.name }}</a>
         </li>
       </ul>
@@ -24,7 +23,7 @@
 export default {
   data() {
     return {
-      navList: [
+      list: [
         { alias: 'top', name: 'TOP' },
         { alias: 'bbs', name: '掲示板' },
         { alias: 'result', name: '大会結果' },
@@ -32,6 +31,12 @@ export default {
         { alias: 'wiki', name: 'Wiki' },
         { alias: 'album', name: 'アルバム' },
         { alias: 'addrbook', name: '名簿' },
+      ],
+      publicList: [
+        { alias: 'top', name: 'TOP' },
+        { alias: 'bbs', name: '掲示板' },
+        { alias: 'schedule', name: '予定表' },
+        { alias: 'wiki', name: 'Wiki' },
       ],
     };
   },
